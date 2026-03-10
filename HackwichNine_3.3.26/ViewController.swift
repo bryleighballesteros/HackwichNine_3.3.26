dfaed5e0aa0be12e6888b78f5602063d37813b78
Bryleigh Ballesteros <bab3@hawaii.edu>
2026-03-10 07:11:16 -1000

    Hackwich Nine - inclass instructor led portion complete and simulator runs

--- HackwichNine_3.3.26/ViewController.swift
+++ HackwichNine_3.3.26/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayLabel.text = ""
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        displayLabel.text = emailTextField.text
+        
+        
+    }
+    
 }
 
